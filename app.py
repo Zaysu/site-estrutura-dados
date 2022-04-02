63c4f85e157876e89a4e52fed8aa3084de7dd802
Zaysu <80471248+Zaysu@users.noreply.github.com>
2022-04-02 19:12:08 -0300

    3.0
    herukoatt

--- app.py
+++ app.py
@@ -16,4 +16,5 @@ def fila_circular():
     return render_template('fila_circular.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
