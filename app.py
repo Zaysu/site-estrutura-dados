9adefed620cb896995227f652575482a48ce6478
Zaysu <80471248+Zaysu@users.noreply.github.com>
2022-04-02 17:10:59 -0300

    2.0
    gunicorn

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+import os
 
 app = Flask(__name__)
 
@@ -16,4 +16,5 @@ def fila_circular():
     return render_template('fila_circular.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
